2202555b02366ad8faa7aeeb32272659865e90ad
olaf.broms@onlinelearning-c.org <olaf.broms@onlinelearning-c.org>
2017-05-13 11:58:05 -0500

    update to check tests

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel for EduforumsNowCodeShip
+                    Laravel for EduforumsNowCodeShip2
                 </div>
 
                 <div class="links">
