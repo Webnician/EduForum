c0eaa09860e1d8f057ff96c01e8b6c87a72f0376
olaf.broms@onlinelearning-c.org <olaf.broms@onlinelearning-c.org>
2017-05-14 01:04:41 -0500

    update to check tests

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel for EduforumsNewTravis1dd
+                    Laravel for EduforumsCapi1
                 </div>
 
                 <div class="links">
