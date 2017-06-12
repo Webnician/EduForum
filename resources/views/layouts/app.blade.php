25873ec410f5d450ad14c3ef14e8dcf1df2eca67
olaf.broms@onlinelearning-c.org <olaf.broms@onlinelearning-c.org>
2017-06-12 08:33:01 -0500

    add google verification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="sEH4hconFUHLP86Qw5gUV273YpouFMsjf6JoUl1gzdk" />
 
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/favicon.png') }}">
     <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}" sizes="32x32">
