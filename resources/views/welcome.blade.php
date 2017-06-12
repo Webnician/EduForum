f2ac30f3be7ab407b4c0b7af62213bd07585b9e0
olaf.broms@onlinelearning-c.org <olaf.broms@onlinelearning-c.org>
2017-06-12 08:43:05 -0500

    add google analytics/verif to home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="google-site-verification" content="sEH4hconFUHLP86Qw5gUV273YpouFMsjf6JoUl1gzdk" />
         <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/favicon.png') }}">
         <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}" sizes="32x32">
         <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}" sizes="16x16">
@@ -17,6 +18,17 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-51786531-6', 'auto');
+            ga('send', 'pageview');
+
+        </script>
 
         <!-- Styles -->
         <style>
