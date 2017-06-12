67a570fb29c405ab56d51c5d8e70f347f5ded30d
olaf.broms@onlinelearning-c.org <olaf.broms@onlinelearning-c.org>
2017-06-12 08:37:41 -0500

    add google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,6 +27,18 @@
             'csrfToken' => csrf_token(),
         ]) !!};
     </script>
+
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-51786531-6', 'auto');
+        ga('send', 'pageview');
+
+    </script>
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <!-- Bootstrap Date-Picker Plugin -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
